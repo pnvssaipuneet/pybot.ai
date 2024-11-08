4e4139ee751d0e48896fc977dbf242cabb12c7e6
pnvssaipuneet <pnvssaipuneet@gmail.com>
2024-11-08 15:43:21 -0500

    Merge remote changes and resolve conflicts

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, send_from_directory
 from PythonTeacherBot import PythonTeacherBot
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 app = Flask(__name__)
 bot = PythonTeacherBot()
 
